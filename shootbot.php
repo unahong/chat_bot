7d84653c59a106a903d5ff4cfc42f28522c9c325
shooooot <butterflymonkey.s@gmail.com>
2017-05-30 22:45:53 +0700

    Edit Sticker

--- shootbot.php
+++ shootbot.php
@@ -10,27 +10,18 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && ($event['message']['type'] == 'text' ||
-        $event['message']['type'] == 'sticker')) {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-      if($event['message']['type'] == 'sticker'){
-        $messages = [
-  				'type' => 'sticker',
-          "packageId": "1",
-          "stickerId": "1"
-  			];
-      }else{
-  			$messages = [
-  				'type' => 'text',
-  				// 'text' => $text
-          'text' => 'ไม่คุย'
-  			];
-      }
+			$messages = [
+				'type' => 'text',
+				// 'text' => $text
+        'text' => 'ไม่คุย'
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
@@ -58,11 +49,11 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				// 'text' => $text
-        'text' => 'ไม่รู้เรื่อง'
-			];
+      $messages = [
+        'type' => 'sticker',
+        "packageId": "1",
+        "stickerId": "1"
+      ];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
