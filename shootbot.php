60efafbbacb354c27e07704cc0003f8e3226c76e
shooooot <butterflymonkey.s@gmail.com>
2017-05-30 22:41:00 +0700

    Add Sticker

--- shootbot.php
+++ shootbot.php
@@ -10,18 +10,27 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		if ($event['type'] == 'message' && ($event['message']['type'] == 'text' ||
+        $event['message']['type'] == 'sticker')) {
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				// 'text' => $text
-        'text' => 'ไม่คุย'
-			];
+      if($event['message']['type'] == 'sticker'){
+        $messages = [
+  				'type' => 'sticker',
+          "packageId": "1",
+          "stickerId": "1"
+  			];
+      }else{
+  			$messages = [
+  				'type' => 'text',
+  				// 'text' => $text
+          'text' => 'ไม่คุย'
+  			];
+      }
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
