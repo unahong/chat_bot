7b7e1cdb99294409f75ecefa2b453bf9ef034533
shooooot <butterflymonkey.s@gmail.com>
2017-05-30 22:04:25 +0700

    shootchat bot

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'U1ee372de08d47b6eaae103c23498b652';
+$access_token = 'FwAFhFr1gV9AJip1HvuAOuKrEtfUCgfxTmFuxuhqnY4scGcPIt6kWgX9ORmLSBEkCr+yUHne2P7cPR/Gz6Q9O6wi9wRrSpEJXClH7Ob4Wq5CscT348Q41aKPMDc8uzWiE1q4p/XXrxPPXZiFJzxuBwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
