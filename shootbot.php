aef218c280324d409854bb4272708c2c17b5a49b
shooooot <butterflymonkey.s@gmail.com>
2017-05-30 22:57:15 +0700

    Fix Bug

--- shootbot.php
+++ shootbot.php
@@ -50,10 +50,14 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
       $messages = [
-        'type' => 'sticker',
-        "packageId": "1",
-        "stickerId": "1"
-      ];
+				'type' => 'text',
+        'text' => 'ไม่รู้'
+			];
+      // $messages = [
+      //   'type' => 'sticker',
+      //   "packageId": "1",
+      //   "stickerId": "1"
+      // ];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
