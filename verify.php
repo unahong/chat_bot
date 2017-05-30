5e2f3a26066a8ddd1764425f19599c3cd784d4f3
shooooot <butterflymonkey.s@gmail.com>
2017-05-30 22:01:50 +0700

    Vertify Token

---
+++ verify.php
@@ -0,0 +1,17 @@
+<?php
+$access_token = 'U1ee372de08d47b6eaae103c23498b652';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
+
+?>
