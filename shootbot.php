377bc66e6f6a6340a1c9795e23cdc3154159d1d2
shooooot <butterflymonkey.s@gmail.com>
2017-05-30 23:00:11 +0700

    Add Sticker reply

--- shootbot.php
+++ shootbot.php
@@ -49,15 +49,15 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-      $messages = [
-				'type' => 'text',
-        'text' => 'ไม่รู้'
-			];
       // $messages = [
-      //   'type' => 'sticker',
-      //   "packageId": "1",
-      //   "stickerId": "1"
-      // ];
+			// 	'type' => 'text',
+      //   'text' => 'ไม่รู้'
+			// ];
+      $messages = [
+        'type' => 'sticker',
+        'packageId'=> '1',
+        'stickerId'=> '1'
+      ];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
